c4b86c9f470f8255fcb676f053915cc2018d5a8b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-01-14 23:59:25 +0200

    Add image

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Balance.module.scss';
+import logo from '../assets/logo.svg';
 
 export default function Chart({ amount }) {
   return (
     <section className={styles.balance}>
-      <div>My balance</div>
-      <p>${amount}</p>
+      <p>My balance</p>
+      <p className={styles.number}>${amount}</p>
     </section>
   );
 }
